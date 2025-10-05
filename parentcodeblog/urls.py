3c09bc4298e15824060816a4a5f1bee830f28e25
Gilberto2025123 <muxindadasilva@gmail.com>
2025-10-05 17:42:45 +0100

    import the parentcode view and url patterns path

--- parentcodeblog/urls.py
+++ parentcodeblog/urls.py
@@ -16,7 +16,9 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from parentcodeapp.views import parentcode_blog
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('parentcodeapp/', parentcode_blog, name='parentcode_blog'),
 ]
