5a98cc085270b233de1e53ec3db76fc919a12c56
Gilberto2025123 <muxindadasilva@gmail.com>
2025-10-05 21:48:18 +0100

    Setting up server( adding installed apps, importing views path, adding url patterns

--- parentcodeblog/settings.py
+++ parentcodeblog/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-%bezcy@8-h$!&so-87b+mr2r$h_u@l(3w%=gzko6w47u&*^q&p'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = True 
 
 ALLOWED_HOSTS = []
 
