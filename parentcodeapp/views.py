39f2ab9a6b4804e7497a69848c3113e43fd59b5c
Gilberto2025123 <muxindadasilva@gmail.com>
2025-10-05 17:36:24 +0100

    Add new app to settings.py and import HttpResponse to views.py for parentcode blog

--- parentcodeapp/views.py
+++ parentcodeapp/views.py
@@ -1,3 +1,5 @@
 from django.shortcuts import render
-
+from django.http import HttpResponse
 # Create your views here.
+def parentcode_blog(request):
+    return HttpResponse("Hello, Tech Parents!")
