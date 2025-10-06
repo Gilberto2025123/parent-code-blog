fe61aa582e17588a5d53f1af123fa1e49ed753ed
Gilberto2025123 <muxindadasilva@gmail.com>
2025-10-06 09:38:48 +0100

    Stop tracking env.py; add env.example and ignore env.py

--- env.py
+++
@@ -1,5 +0,0 @@
-import os
-
-os.environ.setdefault('SECRET_KEY', 'django-insecure-%bezcy@8-h$!&so-87b+mr2r$h_u@l(3w%=gzko6w47u&*^q&p')
-
-os.environ.setdefault("DATABASE_URL","postgresql://neondb_owner:npg_tA9EqyXgC0uS@ep-damp-rain-ag5pc7dh.c-2.eu-central-1.aws.neon.tech/raft_ocean_union_265946",) 
