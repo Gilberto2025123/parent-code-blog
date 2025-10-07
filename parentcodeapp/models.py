6e49b99ded88456330f373486b5e6e44738dfeb4
Gilberto2025123 <muxindadasilva@gmail.com>
2025-10-07 15:08:41 +0100

    Add Post and Category models

--- parentcodeapp/models.py
+++ parentcodeapp/models.py
@@ -1,3 +1,26 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
+
+class Category(models.Model):
+    name = models.CharField(max_length=100, unique=True)
+    slug = models.SlugField(max_length=200, unique=True)
+    def __str__(self):
+        return self.name
+    
+
+class Post(models.Model):
+    title = models.CharField(max_length=200, unique=True)
+    slug = models.SlugField(max_length=200, unique=True)
+    author = models.ForeignKey(
+    User, on_delete=models.CASCADE, related_name="parentcodeapp_posts"
+)
+    content = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    is_published = models.BooleanField(default=True)
+    category = models.ForeignKey(Category, on_delete=models.CASCADE, related_name="parentcodeapp_catergory")
+
+def __str__(self):
+        return self.title
