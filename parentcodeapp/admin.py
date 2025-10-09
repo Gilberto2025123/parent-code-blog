c5eb1c0334ca125e9d23e807ac057d5ac069ba21
Gilberto2025123 <muxindadasilva@gmail.com>
2025-10-09 13:14:49 +0100

    adding content and author to search fields

--- parentcodeapp/admin.py
+++ parentcodeapp/admin.py
@@ -6,7 +6,7 @@ from django_summernote.admin import SummernoteModelAdmin
 class PostAdmin(SummernoteModelAdmin):
 
     list_display = ('title', 'slug', 'status', 'created_at', 'author',)
-    search_fields = ['title']
+    search_fields = ['title', 'content']
     list_filter = ('status', 'created_at', 'author',)
     prepopulated_fields = {'slug': ('title',)}
     summernote_fields = ('content',)
