68b67e423854c041420452616b50647f4f6f20d5
Gilberto2025123 <muxindadasilva@gmail.com>
2025-10-12 13:52:09 +0100

    Add authentication for users

--- parentcodeblog/urls.py
+++ parentcodeblog/urls.py
@@ -19,6 +19,7 @@ from django.urls import path, include
 
 urlpatterns = [
      path('aboutus/', include("aboutus.urls"), name="aboutus-urls"),
+     path("accounts/", include("allauth.urls")),
      path('admin/', admin.site.urls),
      path('summernote/', include('django_summernote.urls')),
      path("", include('parentcodeapp.urls'), name='parentcode_urls'),
