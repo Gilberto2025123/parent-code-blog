af36e03e59cbc34ecd607c300e3c49e0ac636941
Gilberto2025123 <muxindadasilva@gmail.com>
2025-10-13 04:39:12 +0100

    Add forms and views for the contact us page

--- aboutus/forms.py
+++ aboutus/forms.py
@@ -4,11 +4,10 @@ from .models import ContactForm
 class ContactFormModelForm(forms.ModelForm):
     class Meta:
         model = ContactForm
-        fields = 'name', 'email', 'message', 'subject', 'created_at'
+        fields = 'name', 'email', 'message', 'subject',
         labels = {
             'name': 'Your Name',
             'email': 'Your Email',
             'message': 'Your Message',
             'subject': 'Subject',
-            'created_at': 'Date Created',
         }
--- aboutus/views.py
+++ aboutus/views.py
@@ -1,16 +1,33 @@
 from django.shortcuts import render
+from django.contrib import messages
 from .models import AboutUs
+from .models import ContactForm
 # Create your views here.
 
-def about_us(request):
+def about_us():
+
     """
     Renders the About Us page
     """
     about = AboutUs.objects.all().order_by('-last_updated').first()
 
     return render(
-        request,
         "aboutus/about_us.html",
         {"aboutus": about},
     )
 
+def contact_us(request):
+    from .forms import ContactForm as ContactFormModelForm 
+    if request.method == "POST":
+        contact_us_form = ContactFormModelForm(request.POST)
+        if contact_us_form.is_valid():
+            contact_us_form.save()
+            messages.add_message(request, messages.SUCCESS, "Thank you for contacting Parent Code! We will get back to you shortly. Thank you!")
+    return render(
+        request,
+        "aboutus/contact_us.html",
+        {
+            "contact_us": contact_us_form,
+            "contact_form": contact_us_form,
+        }
+    )
