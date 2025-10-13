ab9496b4f1a9a55462272152f65b8eac121b8e2e
Gilberto2025123 <muxindadasilva@gmail.com>
2025-10-13 11:55:36 +0100

    Add Cloudinary

--- parentcodeapp/models.py
+++ parentcodeapp/models.py
@@ -1,6 +1,7 @@
 
 from django.db import models
 from django.contrib.auth.models import User
+from cloudinary.models import CloudinaryField
 
 STATUS = ((0, "Draft"), (1, "Published"))
 
@@ -19,6 +20,7 @@ class Post(models.Model):
     author = models.ForeignKey(
         User, on_delete=models.CASCADE, related_name="parentcodeapp_author"
     )
+    featured_image = CloudinaryField('image', default='placeholder')
     content = models.TextField()
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
