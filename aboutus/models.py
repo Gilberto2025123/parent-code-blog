865752573a71ef96169771aca2365fb1e4eca703
Gilberto2025123 <muxindadasilva@gmail.com>
2025-10-13 04:47:32 +0100

    Move ContactForm model to the aboutus app

--- aboutus/models.py
+++ aboutus/models.py
@@ -8,3 +8,17 @@ class AboutUs(models.Model):
 
     def __str__(self):
         return self.title
+    
+    class ContactForm(models.Model):
+     name = models.CharField(max_length=100, unique=True)
+    email = models.EmailField(max_length=200)
+    subject = models.CharField(max_length=200)
+    message = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    read = models.BooleanField(default=False)
+
+    class Meta:
+        ordering = ["-created_at"]
+
+    def __str__(self):
+        return self.name
--- parentcodeapp/models.py
+++ parentcodeapp/models.py
@@ -63,22 +63,8 @@ class Comment(models.Model):
         return f"Comment: {self.content} by {self.author}"
 
 
-class ContactForm(models.Model):
-    name = models.CharField(max_length=100, unique=True)
-    email = models.EmailField(max_length=200)
-    subject = models.CharField(max_length=200)
-    message = models.TextField()
-    created_at = models.DateTimeField(auto_now_add=True)
-    read = models.BooleanField(default=False)
 
-    class Meta:
-        ordering = ["-created_at"]
-
-    def __str__(self):
-        return self.name
     
-
-
 class Bookmarks(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE, related_name="parentcodeapp_bookmarks")
     post = models.ForeignKey(Post, on_delete=models.CASCADE, related_name="parentcodeapp_bookmarks")
