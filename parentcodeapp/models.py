55dddc4404ecfc0d084a08f5eea53420eaab88dc
Gilberto2025123 <muxindadasilva@gmail.com>
2025-10-13 02:39:48 +0100

    Create  my profile, JobPost, Bookmarks and ContactForm models

--- parentcodeapp/models.py
+++ parentcodeapp/models.py
@@ -63,3 +63,54 @@ class Comment(models.Model):
         return f"Comment: {self.content} by {self.author}"
 
 
+class ContactForm(models.Model):
+    name = models.CharField(max_length=100, unique=True)
+    email = models.EmailField(max_length=200)
+    subject = models.CharField(max_length=200)
+    message = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        ordering = ["-created_at"]
+
+    def __str__(self):
+        return self.name
+    
+
+
+class Bookmarks(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name="parentcodeapp_bookmarks")
+    post = models.ForeignKey(Post, on_delete=models.CASCADE, related_name="parentcodeapp_bookmarks")
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        ordering = ["-created_at"]
+
+    def __str__(self):
+        return f"Blog saved by {self.user}"
+
+class JobPost(models.Model):
+    title = models.CharField(max_length=200)
+    company = models.CharField(max_length=200)
+    location = models.CharField(max_length=200)
+    description = models.TextField()
+    apply_link = models.URLField(max_length=200)
+    created_at = models.DateTimeField(auto_now_add=True)
+    posted_by = models.ForeignKey(User, on_delete=models.CASCADE, related_name="parentcodeapp_jobs")
+    slug = models.SlugField(max_length=200, unique=True)
+
+    class Meta:
+        ordering = ["-created_at"]  
+
+    def __str__(self):
+        return f"{self.title} at {self.company}"
+   
+class MyProfile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    bio = models.TextField(blank=True)
+
+    # profile_picture = models.ImageField(upload_to='profile_pictures/', blank=True, null=True)
+    
+
+    def __str__(self):
+        return f"{self.user.username}'s profile"
