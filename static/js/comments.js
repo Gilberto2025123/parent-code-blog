e439e86ce4d17e4838bcdc70640a470f71ee3c00
Gilberto2025123 <muxindadasilva@gmail.com>
2026-01-17 02:32:01 +0000

    Testing the delete function

--- static/js/comments.js
+++ static/js/comments.js
@@ -30,7 +30,7 @@ for (let button of editButtons) {
 * Initializes deletion functionality for the provided delete buttons.
 * 
 * For each button in the `deleteButtons` collection:
-* - Retrieves the associated comment's ID upon click.
+* - Retrieves the associated comment's ID and post slug upon click.
 * - Updates the `deleteConfirm` link's href to point to the 
 * deletion endpoint for the specific comment.
 * - Displays a confirmation modal (`deleteModal`) to prompt 
@@ -38,8 +38,10 @@ for (let button of editButtons) {
 */
 for (let button of deleteButtons) {
   button.addEventListener("click", (e) => {
-    let commentId = e.target.getAttribute("comment_id");
-    deleteConfirm.href = `delete_comment/${commentId}`;
+    let commentId = e.target.getAttribute("data-comment-id");
+    let slug = e.target.getAttribute("data-slug");
+    // Build the correct delete URL with the slug
+    deleteConfirm.href = `/blog/${slug}/delete_comment/${commentId}`;
     deleteModal.show();
   });
 }
